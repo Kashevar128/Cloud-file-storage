94d4075e4f075754522679ee89c7bf8aa2f2fa42
mstislav <steve128@yandex.ru>
2022-12-27 08:07:26 +0300

    Дз лекция 3 (Порождающие шаблоны)

--- server/src/main/java/org/vinogradov/myserver/serverLogic/connectionService/ConnectionLimit.java
+++ server/src/main/java/org/vinogradov/myserver/serverLogic/connectionService/ConnectionLimit.java
@@ -14,7 +14,11 @@ public class ConnectionLimit {
     private Timer timer;
     private long delay;
 
-    public ConnectionLimit(ChannelHandlerContext userContext) {
+    public static ConnectionLimit of(ChannelHandlerContext context) {
+        return new ConnectionLimit(context);
+    }
+
+    private ConnectionLimit(ChannelHandlerContext userContext) {
         this.userContext = userContext;
         this.delay = 180000L;
         this.timer = new Timer();
